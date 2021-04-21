e92daa2b674798d57182ffa83dbeeeaa58e63fdd
Baylor Molnar <baylor_molnar1@baylor.edu>
2021-04-21 15:17:10 -0500

    Working in class

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 app_name = "MainApp"
-
+# need 3 things: URL(address, view), VIEW(function to interact with database and website), HTML(template file to display code)
 urlpatterns = [
     path("", views.index, name="index"),
+    path("topics", views.topics, name="topics"),
+    path("topics/<int:topic_id>/", views.topic, name="topic"),
+    path("new_topic/", views.new_topic, name="new_topic"),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Topic, Entry
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -6,3 +8,36 @@ from django.shortcuts import render
 def index(request):
     """The home page for learning log"""
     return render(request, "MainApp/index.html")
+
+
+def topics(request):
+    topics = Topic.objects.order_by(
+        "date_added"
+    )  # this is from the model.py file (to get descending you put a - in front
+
+    context = {"topics": topics}
+    return render(request, "MainApp/topics.html", context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by("-date_added")
+
+    context = {"topic": topic, "entries": entries}
+
+    return render(request, "MainApp/topic.html", context)
+
+
+def new_topic(request):
+    if request.method != "POST":
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("MainApp:topics")
+
+    context = {"form": form}
+    return render(request, "MainApp/new_topic.html", context)
